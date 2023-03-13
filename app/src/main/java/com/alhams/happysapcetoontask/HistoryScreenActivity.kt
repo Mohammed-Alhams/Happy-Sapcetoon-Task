46815a37d085ce8bf74517f56f7856672c910c35
Noor <>
2023-03-13 14:20:00 +0200

    create history screen activity

---
+++ app/src/main/java/com/alhams/happysapcetoontask/HistoryScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.alhams.happysapcetoontask
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HistoryScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history_screen)
+    }
+}
